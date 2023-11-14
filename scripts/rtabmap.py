2eb6e00cbb8e53cf7b83a2624e0725be2d31d51d
andrey1908 <andrey.krishtopik@mail.ru>
2023-11-14 20:45:18 +0300

    follow major changes in docker_helper

--- scripts/rtabmap.py
+++ scripts/rtabmap.py
@@ -3,27 +3,24 @@ from docker_helper import DockerMounts, RosDockerContainer
 
 
 class RtabmapMounts(DockerMounts):
-    def __init__(self, files=tuple(), folders=tuple()):
-        super().__init__(files=files, folders=folders)
-        self.pass_rtabmap_to_docker()
-
-    def pass_rtabmap_to_docker(self):
-        rtabmap_ws_folder = (osp.join(osp.dirname(__file__), '../../..'))
-        if not osp.isdir(osp.join(rtabmap_ws_folder, 'src')):
-            raise RuntimeError("Looks like rtabmap_example package is not in 'src' folder")
-        self.docker_rtabmap_ws_folder, volume = \
-            DockerMounts.pass_folders_to_docker(rtabmap_ws_folder, '/home/docker_rtabmap/catkin_ws')
-        self.volume_args = self.volume_args + f'-v {volume} '
+    def __init__(self):
+        super().__init__()
+        host_ws_folder = osp.join(osp.dirname(__file__), '../../..')
+        self.add_folder(host_ws_folder, "/home/docker_rtabmap/catkin_ws")
 
 
 class Rtabmap(RosDockerContainer):
-    def __init__(self, image_name, container_name, user_name=None):
+    def __init__(self, image_name="rtabmap:latest", container_name="rtabmap",
+            user_name=None):
         super().__init__(image_name, container_name, user_name=user_name)
-        self.source_files = ['/home/docker_rtabmap/catkin_ws/devel_isolated/setup.bash']
+        self.source_files = [osp.join(osp.dirname(__file__), "../../../devel_isolated/setup.bash")]
 
     def create_containter(self, mounts: RtabmapMounts=None, net='host'):
         if mounts is None:
             mounts = RtabmapMounts()
+        if type(mounts) is not RtabmapMounts:
+            raise RuntimeError(f"Type of 'mounts' argument should be RtabmapMounts, not {type(mounts)}")
+
         super().create_containter(mounts=mounts, net=net)
 
     def build_rtabmap(self):
@@ -35,14 +32,31 @@ class Rtabmap(RosDockerContainer):
             save_tracking_results_path=None,
             optimization_results_topic=None, node_name=None,
             use_semantic=False):
+        arguments = str()
+
+        config_paths = list(map(self.resolve, config_paths))
+        arguments += f"config_paths:={','.join(config_paths)} "
+
+        if load_map_path is not None:
+            load_map_path = self.resolve(load_map_path)
+            arguments += f"load_map_path:={load_map_path} "
+
+        if save_map_path is not None:
+            save_map_path = self.resolve(save_map_path)
+            arguments += f"save_map_path:={save_map_path} "
+
+        if save_tracking_results_path is not None:
+            save_tracking_results_path = self.resolve(save_tracking_results_path)
+            arguments += f"save_tracking_results_path:={save_tracking_results_path} "
+
+        if optimization_results_topic is not None:
+            arguments += f"optimization_results:={optimization_results_topic} "
+
+        if node_name is not None:
+            arguments += f"node_name:={node_name} "
+
+        arguments += f"accum/subscribe_rgb:={use_semantic} "
+
         result = self.roslaunch("rtabmap_example", "occupancy_grid_map.launch",
-            arguments=
-                (f"config_paths:={','.join(config_paths)} ") +
-                (f"load_map_path:={load_map_path} " if load_map_path else "") +
-                (f"save_map_path:={save_map_path} " if save_map_path else "") +
-                (f"save_tracking_results_path:={save_tracking_results_path} " if save_tracking_results_path else "") +
-                (f"optimization_results:={optimization_results_topic} " if optimization_results_topic else "") +
-                (f"node_name:={node_name} " if node_name else "") +
-                (f"accum/subscribe_rgb:={use_semantic}"),
-            source_files=self.source_files)
+            arguments=arguments, source_files=self.source_files)
         return result
--- scripts/run_rtabmap.py
+++ scripts/run_rtabmap.py
@@ -26,17 +26,6 @@ def build_parser():
     return parser
 
 
-def getPathInsideDocker(path: str,
-        catkin_ws_folder: str, docker_catkin_ws_folder: str):
-    full_path = osp.abspath(osp.expanduser(path))
-    if not full_path.startswith(catkin_ws_folder + '/'):
-        raise RuntimeError(f"Load map file {path} should be in "
-            f"{catkin_ws_folder} folder")
-    docker_path = osp.join(docker_catkin_ws_folder,
-        osp.relpath(full_path, catkin_ws_folder))
-    return docker_path
-
-
 def removeOldRosbagLogFiles(folder, max_total_size):  # in MB
     files = sorted(glob.glob(folder + "/*.bag") + glob.glob(folder + "/*.bag.active"))
     sizes = list(map(osp.getsize, files))
@@ -66,7 +55,7 @@ def run_rtabmap(**kwargs):
     args = parser.parse_args()
     args.__dict__.update(kwargs)
 
-    rtabmap = Rtabmap('rtabmap:latest', 'rtabmap')
+    rtabmap = Rtabmap()
     rtabmap.create_containter()
 
     # build and exit
@@ -74,41 +63,16 @@ def run_rtabmap(**kwargs):
         rtabmap.build_rtabmap()
         exit(0)
 
-    catkin_ws_folder = osp.abspath(osp.join(osp.dirname(__file__), "../../.."))
-    if not osp.isdir(osp.join(catkin_ws_folder, 'src')):
-        raise RuntimeError("Looks like rtabmap_example package is not in 'src' folder")
-    docker_catkin_ws_folder = "/home/docker_rtabmap/catkin_ws"
-
-    config_paths = [osp.join(docker_catkin_ws_folder,
-        "src/rtabmap_example/config/husky.yaml")]
-    if not args.local_mapping:
-        node_name = "occupancy_grid_map"
-    else:
-        config_paths.append(osp.join(docker_catkin_ws_folder,
-            "src/rtabmap_example/config/husky_enable_local_mapping.yaml"))
+    config_paths = [osp.join(osp.dirname(__file__), "../config/husky.yaml")]
+    if args.local_mapping:
+        config_paths.append(osp.join(osp.dirname(__file__), "../config/husky_enable_local_mapping.yaml"))
         node_name = "occupancy_grid_local_map"
-
-    if args.load_map:
-        load_map_path = getPathInsideDocker(args.load_map,
-            catkin_ws_folder, docker_catkin_ws_folder)
     else:
-        load_map_path = None
-
-    if args.save_map:
-        save_map_path = getPathInsideDocker(args.save_map,
-            catkin_ws_folder, docker_catkin_ws_folder)
-    else:
-        save_map_path = None
-
-    if args.save_tracking_results:
-        save_tracking_results_path = getPathInsideDocker(args.save_tracking_results,
-            catkin_ws_folder, docker_catkin_ws_folder)
-    else:
-        save_tracking_results_path = None
+        node_name = "occupancy_grid_map"
 
     time_str = time.strftime("%Y-%m-%d_%H.%M.%S")
+    catkin_ws_folder = osp.abspath(osp.join(osp.dirname(__file__), "../../.."))
     logs_folder = osp.abspath(osp.expanduser(osp.join(catkin_ws_folder, "rtabmap_logs")))
-    docker_logs_folder = osp.join(docker_catkin_ws_folder, "rtabmap_logs")
     os.makedirs(logs_folder, exist_ok=True)
 
     if args.log_rosbag:
@@ -116,13 +80,13 @@ def run_rtabmap(**kwargs):
         topics_to_record = ['/tf', '/tf_static',
             '/cartographer/tracked_local_odometry', '/cartographer/tracked_global_odometry',
             '/cartographer/trajectory_node_list', '/cartographer/constraint_list']
-        docker_out_rosbag_log_file = osp.join(docker_logs_folder, f"{time_str}.bag")
+        docker_out_rosbag_log_file = rtabmap.resolve(osp.join(logs_folder, f"{time_str}.bag"))
         rtabmap.rosrun_async("rosbag", "record",
             arguments=f"{' '.join(topics_to_record)} -O {docker_out_rosbag_log_file}", session='rtabmap_rosbag_log')
 
     results = rtabmap.run_rtabmap(config_paths,
-        load_map_path=load_map_path, save_map_path=save_map_path,
-        save_tracking_results_path=save_tracking_results_path,
+        load_map_path=args.load_map, save_map_path=args.save_map,
+        save_tracking_results_path=args.save_tracking_results,
         node_name=node_name, use_semantic=args.use_semantic)
 
     if args.log_rosbag:
